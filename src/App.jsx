4dc2e7a4fb7658181768dbcafbdb4737c5a83ab4
axiu <abawor200@gmail.com>
2024-02-26 06:39:26 +0000

    Added other cards and rendered them using .map

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,27 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
+    const cards = data.map(card => {
+        return (
+            <Card
+                key={card.id}
+                coverImg={card.coverImg}
+                rating={card.stats.rating}
+                reviewCount={card.stats.reviewCount}
+                location={card.location}
+                title={card.title}
+                price={card.price}
+            />
+        )
+    })
     return (
         /* <Hero />*/
         <div>
             <Navbar />
-            <Card 
-                img = {"katie-zaferes.png"}
-                rating = {"5.0"}
-                reviewCount = {6}
-                country = {"Whatever you want"}
-                title = {"Life Lessons with Katie Zaferes"}
-                price = {136}
-            />
+            {cards}
         </div>
     )
 }
-
-
-/*
-let jokes = [["a", "aa"], ["b", "bb"]]
-
-function renderJokes(jokes) {
-    let jokesList = []
-    for (let i = 0; i < jokes.length; i++) {
-        jokesList.push(
-            <Joke
-                key={i}
-                setup={jokes[i][0]}
-                punchline={jokes[i][1]}
-            />
-        )
-    }
-    return jokesList
-}
-
-export default function App() {
-    return(
-        <div>            
-            {renderJokes(jokes)}
-        </div>
-    )
-}
-*/
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,30 +3,15 @@ import React from "react"
 export default function Card(props) {
     return (
         <div className="card">
-            <img src="src/images/katie-zaferes.png" className="card--image" />
+            <img src={`src/images/${props.coverImg}`} className="card--image" />
             <div className="card--stats">
-                <img src={props.img} className="card--star" />
+                <img src="src/images/star.png" className="card--star" />
                 <span>{props.rating} </span>
                 <span className="gray">({props.reviewCount}) - </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
-
-
-/*
-import React from "react"
-
-export default function Joke(props) {
-    return(
-        <div>
-            <h2>Setup: {props.setup}</h2>
-            <p>Punchline: {props.punchline}</p>
-            <hr></hr>
-        </div>
-    )
-}
-*/
